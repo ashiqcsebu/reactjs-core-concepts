b45da8dff95ee6c4e7db5283b5bb6925aef5f7c2
ashiqcsebu <ashiq.cse3.bu@gmail.com>
2022-09-20 11:48:10 +0600

    react component practice

--- src/App.js
+++ src/App.js
@@ -2,24 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const students =[
+    {name :'abul' , roll : 32 , address : 'dhaka'},
+    {name :'babul' , roll : 32 , address : 'dhaka'},
+    {name :'cabul' , roll : 32 , address : 'dhaka'},
+    {name :'dabul' , roll : 32 , address : 'dhaka'},
+    {name :'mabul' , roll : 32 , address : 'dhaka'},
+
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+       
+           students.map(student => <Person name={student.name} roll= {student.address}></Person> )
+      }
     </div>
+
   );
+
 }
+
+function Person(props){
+  return (
+    <div>
+       <h1>Name : {props.name} </h1>
+      <h2>Address: {props.roll} </h2>  
+     </div>    
+  );
+ 
+}
+
+
+
 
 export default App;
